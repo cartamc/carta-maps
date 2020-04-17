0198f31aed8918bff7086bd3d9053182df8b46de
Kevin Liu <kevin@kliu.io>
2020-04-17 18:55:56 -0400

    Add reference to BungeePerms

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,12 +9,14 @@ repositories {
     mavenCentral()
     jcenter()
     maven("https://oss.sonatype.org/content/repositories/snapshots")
+    maven("https://repo.wea-ondara.net/repository/public/")
 }
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("com.mattmalec.Pterodactyl4J:Pterodactyl4J:1.0")
     compileOnly("net.md-5:bungeecord-api:1.15-SNAPSHOT")
+    compileOnly("net.alpenblock:BungeePerms:4.0-dev-97")
 }
 
 tasks {
